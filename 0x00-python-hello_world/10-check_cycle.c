86cdf64d38c33de1c9262f6250c7a731fa39a49d
Hilly-man <hilkiahe@gmail.com>
2023-06-18 14:53:55 -0700

    Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks for linked list contained in a cycle
+ * @list: linked list to be check
+ * Return: 1 if the list has cycle, 0 if not
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
