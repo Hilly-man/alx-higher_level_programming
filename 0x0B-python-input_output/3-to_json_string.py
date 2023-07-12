b2080d25f812c193f883a2d8901fbeb34a18007c
Hilly-man <hilkiahe@gmail.com>
2023-07-12 16:19:52 -0700

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines string-to-JSON function."""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation in a string object."""
+    return json.dumps(my_obj)
