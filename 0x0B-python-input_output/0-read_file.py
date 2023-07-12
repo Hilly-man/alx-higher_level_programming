eef2cd97f4f8c375d563b6608c0f44b675515696
Hilly-man <hilkiahe@gmail.com>
2023-07-12 16:10:27 -0700

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,11 @@
   """Defines text file-read funcation."""
 
 
-  def read_file(filename=""):
-      """Prints content of a UTF8 text file to stdout."""
-      with open(flename, encoding="utf-8") as f:
-          print(f.read(), end="")
+def read_file(filename=""):
+    """Prints content of a UTF8 text file to stdout."""
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
+
+read_file("example.txt")
+
