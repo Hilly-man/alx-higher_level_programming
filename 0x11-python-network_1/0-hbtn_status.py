181adb7cfad68ffd7f8e595533da38596bea1f35
Hilly-man <hilkiahe@gmail.com>
2023-09-29 16:53:39 -0700

    What's my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,32 +1,14 @@
 #!/usr/bin/python3
-"""
-This Python script fetches the content of a specified URL using the urllib
-package and displays information about the response.
-
-Requirements:
-- You must use the package urllib.
-- You are not allowed to import any packages other than urllib.
-- The body of the response must be displayed like the following example
-    (tabulation before -)
-- You must use a with statement
-"""
-
+"""Fetching https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-# Define the URL to fetch
-url = 'https://alx-intranet.hbtn.io/status'
 
-try:
-    # Open the URL using a with statement to ensure proper resource management
-    with urllib.request.urlopen(url) as response:
-        # Read the content of the response
-        content = response.read()
-
-        # Display information about the response
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type:", type(content))
-        print("\t- content:", content.decode('utf-8'))
-except urllib.error.URLError as:
-    # Handle URL errors and display an error message
-    print("Error:", e.reason)
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
 
