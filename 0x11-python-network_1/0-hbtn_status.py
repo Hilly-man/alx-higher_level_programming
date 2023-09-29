a000abf85fd0851293fae0af8e4d0cb159bb67c3
Hilly-man <hilkiahe@gmail.com>
2023-09-29 06:44:57 -0700

    What's my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
-This Python script fetches the content of a specified URL using the urllib package
-and displays information about the response.
+This Python script fetches the content of a specified URL using the urllib
+package and displays information about the response.
 
 Requirements:
 - You must use the package urllib.
 - You are not allowed to import any packages other than urllib.
-- The body of the response must be displayed like the following example (tabulation before -)
+- The body of the response must be displayed like the following example
+    (tabulation before -)
 - You must use a with statement
 """
 
@@ -20,12 +21,11 @@ try:
     with urllib.request.urlopen(url) as response:
         # Read the content of the response
         content = response.read()
-        
+
         # Display information about the response
         print("Body response:")
         print("\t- type:", type(content))
         print("\t- content:", content.decode('utf-8'))
-except urllib.error.URLError as e:
+except urllib.error.URLError as:
     # Handle URL errors and display an error message
     print("Error:", e.reason)
-
