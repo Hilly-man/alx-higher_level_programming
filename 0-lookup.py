9283d7d18bf700fe459f5ae8bb091af2a3842f0c
Hilly-man <hilkiahe@gmail.com>
2023-09-29 06:35:47 -0700

    Readme file

--- 0-lookup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-       """Defines object attribute lookup function."""
-
-
-def lookup(obj):
-    """Return a list of object's available attributes."""
-    return (dir(obj))
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x0A-python-inheritance
+alx-higher_level_programming
