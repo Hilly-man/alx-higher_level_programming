f53ed4b687a265f8adacdb5bf3e84a2bbdcc794a
Hilly-man <hilkiahe@gmail.com>
2023-09-29 17:17:44 -0700

    What's my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,6 @@
 """Fetching https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-
 if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
@@ -11,4 +10,3 @@ if __name__ == "__main__":
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
-
