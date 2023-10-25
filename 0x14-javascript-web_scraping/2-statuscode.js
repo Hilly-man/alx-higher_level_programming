a24a48a09617e27233e3eb34228abfdc02437b78
Hilly-man <hilkiahe@gmail.com>
2023-10-25 05:30:59 -0700

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-const id = process.argv[2];
-
-request(`https://swapi-api.alx-tools.com/api/films/${id}`, { json: true }, (err, res, body) => {
-  if (err) console.log(err);
-  console.log(body.title);
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
